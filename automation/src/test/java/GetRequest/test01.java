793ada7c028db7f43d71b5e8883b4be2215a74af
Kpath33~ <kartikeypathak14@gmail.com>
2025-01-28 19:27:43 +0530

    Add initial project structure with Maven configuration and sample tests

---
+++ automation/src/test/java/GetRequest/test01.java
@@ -0,0 +1,23 @@
+package GetRequest;
+
+import org.testng.annotations.Test;
+
+import java.net.http.HttpRequest;
+
+import io.restassured.RestAssured;
+
+public class test01 {
+    @Test
+    public void test01() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api/users/2";
+        RestAssured.given().get().then().statusCode(200).log().all();
+        if (RestAssured.given().get().getStatusCode() == 200) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+            
+        }
+    }
+
+}
